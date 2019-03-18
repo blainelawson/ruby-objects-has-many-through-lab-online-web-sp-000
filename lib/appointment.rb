3492c57d9d1e2db23508bc6c2fa863194a6d8697
Blaine Lawson <blaine.lawson@gmail.com>
2019-03-18 20:38:52 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,8 +1,12 @@
 class Appointment
   @@all = []
+  attr_accessor :date, :patient, :doctor
 
   def initialize(date, patient, doctor)
-
+    @date = date
+    @patient = patient
+    @doctor = doctor
+    
     @@all << self
   end
 
