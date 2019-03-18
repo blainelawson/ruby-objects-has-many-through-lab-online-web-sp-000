40686950bacb95131c1c87e7f94ef07b88ae2447
Blaine Lawson <blaine.lawson@gmail.com>
2019-03-18 20:51:39 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -14,16 +14,11 @@ class Patient
   end
 
   def appointments
-    Appointment.all.select do |appointment|
-
-      appointment.patient == self
-    end
+    Appointment.all.select {|appointment| appointment.patient == self}
   end
 
   def doctors
-    appointments.collect do |appointment|
-      appointment.doctor
-    end
+    appointments.collect {|appointment| appointment.doctor}
   end
 
   def self.all
